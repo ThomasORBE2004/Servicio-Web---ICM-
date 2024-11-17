9e9b1f76dd76ed84dd32f711f2989c89698c2e43
Thomas <thomasorbe2004@gmail.com>
2024-11-16 23:17:24 -0500

    Actualizado Configuracion Firebase

--- servicio_web/src/environments/environment.ts
+++ servicio_web/src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'modoviaje2430',
+    appId: '1:614083887141:web:131df082a40bc50054ffcc',
+    databaseURL: 'https://modoviaje2430-default-rtdb.firebaseio.com',
+    storageBucket: 'modoviaje2430.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA6uegrnSDnnOCFs1ea-IlWlQmjFGtTnqY',
+    authDomain: 'modoviaje2430.firebaseapp.com',
+    messagingSenderId: '614083887141',
+  },};
