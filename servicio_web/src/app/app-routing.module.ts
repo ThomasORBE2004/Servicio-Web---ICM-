3a9cce2fe56a77ffd2186a6084283344c28c388d
Thomas <thomasorbe2004@gmail.com>
2024-11-19 01:35:04 -0500

    Añadir Archivos como ruta

--- servicio_web/src/app/app-routing.module.ts
+++ servicio_web/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComentariosListComponent } from './comentarios-list/comentarios-list.component';
 import { HomeComponent } from './home/home.component';
+import { ArchivosComponent } from './archivos/archivos.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'comentarios', component: ComentariosListComponent }
+  { path: 'comentarios', component: ComentariosListComponent },
+  { path: 'archivos', component: ArchivosComponent}
 ];
 
 @NgModule({
